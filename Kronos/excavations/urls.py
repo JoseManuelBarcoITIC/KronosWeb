5681149e74c68a64021e173f72d85a4e612e8f74
JoseManuelBarcoITIC <2526_jose.barco@iticbcn.cat>
2026-05-13 16:42:12 +0200

    Arreglado pequeño error en urls

--- Kronos/excavations/urls.py
+++ Kronos/excavations/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('excavations/', views.excavation_list, name='excavation-list'),
     path('excavations/<int:pk>/', views.excavation_detail, name='excavation-detail'),
 
-    # Rutas para Sectores
     path('sectors/', views.sector_list, name='sector-list'),
     path('sectors/<int:pk>/', views.sector_detail, name='sector-detail'),
 ]
